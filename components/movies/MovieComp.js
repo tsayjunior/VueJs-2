c9e9a8f0105e38a3cf3185c1c89706669e0556c3
tsayjunior <juniorllanos15@gmail.com>
2022-08-10 17:49:14 -0400

    save

--- components/movies/MovieComp.js
+++ components/movies/MovieComp.js
@@ -1,12 +1,17 @@
+// actualizar desde el componente hijo al componente padre con $emit
 let MovieComp={
     template:`
         <div>
             <img :src="cover">
             <h2 v-text="title"></h2>
             <p v-text="synopsis"></p>
+            <button @click="toggleLike" v-text="like ? 'Favorita' : 'Agregar a favorita' "></button>
+
             <hr>
         </div>
     `,
+    // <button @click="$emit('update:like', !like)" v-text="like ? 'Favorita' : 'Agregar a favorita' "></button>
+
     // props:[
     //     'id',
     //     'title',
@@ -29,6 +34,23 @@ let MovieComp={
         cover: {
             type: String,
             required: true,
+        },
+        like:{
+            type:Boolean,
+            required:true,
+            default(){
+                return false
+            }
+        }
+    },
+    methods:{
+        toggleLike(){
+            // this.like = !this.like
+            let data = {
+                id: this.id,
+                like: !this.like,
+            }
+            this.$emit('toggleLike', data)
         }
     }
 }
